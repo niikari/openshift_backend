3f3c10934ebee5fce04b8a5cf69d7c7760f16fc3
niikari <pakniiles@gmail.com>
2022-04-08 11:22:34 +0300

    mahdollistettu cors kaikista ip-osoitteista

--- src/main/java/com/tehtava/openshift/repositories/KotityoRepository.java
+++ src/main/java/com/tehtava/openshift/repositories/KotityoRepository.java
@@ -1,9 +1,13 @@
 package com.tehtava.openshift.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.tehtava.openshift.models.Kotityo;
 
+@RepositoryRestResource
+@CrossOrigin(origins ="*")
 public interface KotityoRepository extends CrudRepository<Kotityo, Long> {
 
 }
