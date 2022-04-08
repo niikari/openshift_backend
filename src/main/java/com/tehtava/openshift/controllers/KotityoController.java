a9cfeed6a0e7d1a65548f5124c914d4dfa9e65e3
niikari <pakniiles@gmail.com>
2022-04-08 13:08:22 +0300

    muokkauksia + mahdollisuus muokata kotityon statusta

--- src/main/java/com/tehtava/openshift/controllers/KotityoController.java
+++ src/main/java/com/tehtava/openshift/controllers/KotityoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,13 +30,26 @@ public class KotityoController {
 	
 	@PostMapping("/add")
 	public Kotityo addKotityo(@RequestBody Kotityo kotityo) {
+		System.out.println("tuli tänne");
 		return this.kotityoRepository.save(kotityo);
 	}
 	
 	@DeleteMapping("/all/{id}")
-	public Optional<Kotityo> deleteKotityo(@PathVariable Long id) {
-		Optional<Kotityo> kt = this.kotityoRepository.findById(id);
+	public Kotityo deleteKotityo(@PathVariable Long id) {
+		Kotityo kt = this.kotityoRepository.findById(id).get();
 		this.kotityoRepository.deleteById(id);
 		return kt;
+	}
+	
+	@PutMapping
+	public Kotityo changeStatus(@PathVariable Long id) {
+		Kotityo kt = this.kotityoRepository.findById(id).get();
+		Boolean tila = kt.getTila();
+		if (tila) {
+			kt.setTila(false);
+		} else {
+			kt.setTila(true);
+		}
+		return this.kotityoRepository.save(kt);
 	}
 }
